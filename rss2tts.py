08e1aa6869b51e2c2ef5b2cd6777daadf0f0eb90
Rui Catarino <55796280+ruitcatarino@users.noreply.github.com>
2022-12-22 04:20:36 +0000

    Detects language and changes tts to that language

--- rss2tts.py
+++ rss2tts.py
@@ -1,6 +1,8 @@
 import pyttsx3
 import feedparser
 import html2text
+import re
+from langdetect import detect
 
 def change_voice(engine, language):
     for voice in engine.getProperty('voices'):
@@ -16,29 +18,54 @@ def change_voice(engine, language):
 def end_voice(engine):
     engine.stop()
 
-def tts(engine,toread):
-
-    print(toread)
+def tts(engine,toread,language):
+    change_voice(engine,language)
 
     engine.say(toread)
 
     engine.runAndWait()
 
-if __name__ == "__main__":
-    
-    #Create object (voice)
-    engine = pyttsx3.init()
+def detect_language(toread):
+    language = detect(toread)
 
-    change_voice(engine,"PT-BR")
-    engine.setProperty('rate', 200)
-    
-    NewsFeed = feedparser.parse("https://www.rtp.pt/noticias/rss")
+    if language == "pt":
+        print("Texto em Português!")
+        return "PT-BR"
+    else:
+        print("Text in English!")
+        return "EN-US"
+
+def rsstotext(url):
+    NewsFeed = feedparser.parse(url)
     entry = NewsFeed.entries[1]
 
     text_maker = html2text.HTML2Text()
     text_maker.ignore_links = True
     text_maker.body_width = 0
 
-    tts(engine,text_maker.handle(entry.summary))
+    toread = text_maker.handle(entry.summary)
+
+    toread = toread.split("![](")
+
+    toread = ''.join(toread)
+
+    toread = re.sub(r'http\S+', '', toread)
+
+    print(toread)
+
+    return toread
+
+if __name__ == "__main__":
+    
+    #Create object (voice)
+    engine = pyttsx3.init()
+
+    engine.setProperty('rate', 180)
+
+    toread = rsstotext("https://www.rtp.pt/noticias/rss")
+
+    language = detect_language(toread)
+
+    tts(engine,toread,language)
 
     end_voice(engine)
